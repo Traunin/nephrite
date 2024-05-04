2698b33c5343512ae677b06c29f6de6fb119468f
Traunin <traunin5@gmail.com>
2024-05-04 08:31:02 +0300

    style: center calendar; change arrows icons

--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -1,8 +1,13 @@
 <template>
-  <button @click.prevent="changeMonth(-1)">&larr;</button>
-  {{ displayedMonth }} {{ year }}
-  <button @click.prevent="changeMonth(+1)">&rarr;</button>
-  <month-calendar :month="month" :year="year"></month-calendar>
+  <div class="calendar">
+    <div class="calendar-controller">
+      <button @click.prevent="changeMonth(-1)">&lt;</button>
+      <span @click="resetDate()">{{ displayedMonth }} {{ year }}</span>
+      <button @click.prevent="changeMonth(+1)">&gt;</button>
+    </div>
+
+    <month-calendar :month="month" :year="year"></month-calendar>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -24,13 +29,58 @@ const months: string[] = [
   'Декабрь',
 ]
 
-let currentDate: Date = new Date()
+const currentDate: Date = new Date()
 const year = ref(currentDate.getFullYear())
 const month = ref(currentDate.getMonth())
 
 const displayedMonth = computed(() => months[month.value])
 
-function changeMonth(offset: number) {
-  month.value = (month.value + offset) % 12
+function changeMonth(offset: number): void {
+  const offsetValue = month.value + offset
+  if (offsetValue > 11) {
+    year.value++
+    month.value = 0
+  } else if (offsetValue < 0) {
+    year.value--
+    month.value = 11
+  } else {
+    month.value = offsetValue
+  }
+}
+
+function resetDate(): void {
+  const currentDate: Date = new Date()
+  year.value = currentDate.getFullYear()
+  month.value = currentDate.getMonth()
 }
 </script>
+
+<style scoped>
+.calendar {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+}
+
+.calendar-controller {
+  display: flex;
+  gap: 10px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.calendar-controller span {
+  width: 120px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.calendar-controller button {
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: var(--text-color);
+  font-size: 1.3em;
+}
+</style>
