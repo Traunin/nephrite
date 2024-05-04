8f7896b8b2f483363f96ed0217ac00bad95f06d8
Traunin <traunin5@gmail.com>
2024-05-04 04:22:50 +0300

    feat: add calendar; add month selector

--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -1,3 +1,36 @@
-<template>Timetable</template>
+<template>
+  <button @click.prevent="changeMonth(-1)">&larr;</button>
+  {{ displayedMonth }} {{ year }}
+  <button @click.prevent="changeMonth(+1)">&rarr;</button>
+  <month-calendar :month="month" :year="year"></month-calendar>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import MonthCalendar from '@/components/MonthCalendar.vue'
+
+const months: string[] = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь',
+]
+
+let currentDate: Date = new Date()
+const year = ref(currentDate.getFullYear())
+const month = ref(currentDate.getMonth())
+
+const displayedMonth = computed(() => months[month.value])
+
+function changeMonth(offset: number) {
+  month.value = (month.value + offset) % 12
+}
+</script>
