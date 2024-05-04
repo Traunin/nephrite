705c2898b4a4cbafe8d6a8272348aad8f35adb6c
Traunin <traunin5@gmail.com>
2024-05-04 08:30:06 +0300

    feat: add current day highlight; add styles for days outside of current month

--- src/components/MonthCalendar.vue
+++ src/components/MonthCalendar.vue
@@ -10,18 +10,77 @@
       <th>Вс</th>
     </tr>
     <tr v-for="(row, index) in calendar" :key="index">
-      <td v-for="date in row" :key="date">{{ date }}</td>
+      <td
+        v-for="date in row"
+        :key="date"
+        :class="{
+          surroundingMonth:
+            (index == 0 && date > 20) || (index > 2 && date < 10),
+          currentDay: props.month == month && props.year == year && date == day,
+        }"
+      >
+        {{ date }}
+      </td>
     </tr>
   </table>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { generateCalendar } from '@/utils/calendar'
 const props = defineProps<{
   month: number
   year: number
 }>()
 
+const day = ref(0)
+const month = ref(0)
+const year = ref(0)
+
+setInterval(() => {
+  updateTime()
+}, 1000)
+
+updateTime()
+
+function updateTime(): void {
+  let currentDate: Date = new Date()
+  day.value = currentDate.getDate()
+  month.value = currentDate.getMonth()
+  year.value = currentDate.getFullYear()
+}
+
 const calendar = computed(() => generateCalendar(props.month + 1, props.year))
 </script>
+
+<style scoped>
+td {
+  padding: 3px 11px 12px 11px;
+  text-align: center;
+  font-size: 0.8em;
+  cursor: pointer;
+  border-radius: 5px;
+  -webkit-user-select: none; /* Safari */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE10+/Edge */
+  user-select: none; /* Standard */
+}
+
+td:hover {
+  background: rgb(63, 63, 63);
+  transition: background 0.05s;
+}
+
+th {
+  padding-bottom: 10px;
+  font-size: 0.8em;
+}
+
+.surroundingMonth {
+  color: rgb(76, 76, 76);
+}
+
+.currentDay {
+  color: rgb(138, 92, 245);
+}
+</style>
