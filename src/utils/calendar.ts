1a031e21bbc8a88421ca8bf88acec76453c4b8e5
Traunin <traunin5@gmail.com>
2024-05-04 04:22:17 +0300

    feat: add calendar.ts for generating a calendar sheet

---
+++ src/utils/calendar.ts
@@ -0,0 +1,72 @@
+export function generateCalendar(month: number, year: number): number[][] {
+  let daysInAPreviousMonth: number = getDaysInAMonth(month - 1, year)
+  const daysInCurrentMonth: number = getDaysInAMonth(month, year)
+  const dayOfTheWeek: number = getDayOfTheWeek(month, year)
+
+  const lines = Math.ceil((dayOfTheWeek + daysInCurrentMonth) / 7)
+  const calendarSheet: number[][] = new Array(lines)
+    .fill(0)
+    .map(() => new Array(7).fill(0))
+
+  for (let i: number = dayOfTheWeek - 1; i >= 0; i--) {
+    calendarSheet[0][i] = daysInAPreviousMonth
+    daysInAPreviousMonth--
+  }
+
+  let r = 0
+  let c: number = dayOfTheWeek
+
+  for (let day = 1; day <= daysInCurrentMonth; day++) {
+    calendarSheet[r][c] = day
+    c++
+    r = r + Math.floor(c / 7)
+    c %= 7
+  }
+
+  let day = 1
+  for (; c <= 6 && r < lines; c++) {
+    calendarSheet[r][c] = day
+    day++
+  }
+  return calendarSheet
+}
+
+function getDaysInFebruary(year: number): number {
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+    return 29
+  }
+  return 28
+}
+
+function getDaysInAMonth(month: number, year: number): number {
+  if (month == 4 || month == 6 || month == 9 || month == 11) {
+    return 30
+  } else if (month == 2) {
+    return getDaysInFebruary(year)
+  }
+  return 31
+}
+
+function getDayOfTheWeek(month: number, year: number): number {
+  if (month < 3) {
+    year--
+  }
+  month = ((month + 9) % 12) + 1
+  return (
+    (floorMod(
+      1 +
+        Math.floor(2.6 * month - 0.2) -
+        2 * Math.floor(year / 100) +
+        (year % 100) +
+        (year % 100) / 4 +
+        Math.floor(year / 400),
+      7
+    ) +
+      6) %
+    7
+  )
+}
+
+function floorMod(x: number, y: number): number {
+  return ((Math.floor(x) % y) + y) % y
+}
