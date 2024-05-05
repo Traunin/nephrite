39a4bb268a63aeef395e9b044a31438846be0bb3
Traunin <traunin5@gmail.com>
2024-05-05 22:45:22 +0300

    feat: create CalendarDate type

---
+++ src/utils/calendar-date.ts
@@ -0,0 +1,33 @@
+export type CalendarDate = {
+  day: number
+  month: number
+  year: number
+}
+
+export function getDate(
+  day: number,
+  month: number,
+  year: number
+): CalendarDate {
+  if (month > 11) {
+    year++
+    month = 0
+  } else if (month < 0) {
+    year--
+    month = 11
+  }
+
+  return {
+    day: day,
+    month: month,
+    year: year,
+  }
+}
+
+export function isSameDate(date1: CalendarDate, date2: CalendarDate): boolean {
+  return (
+    date1.day == date2.day &&
+    date1.month == date2.month &&
+    date1.year == date2.year
+  )
+}
