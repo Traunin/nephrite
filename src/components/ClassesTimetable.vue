c490cfe374604d5aab30ddd1bce985c778c54a2b
Traunin <traunin5@gmail.com>
2024-05-09 23:35:17 +0300

    feat: add timetable

---
+++ src/components/ClassesTimetable.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="week">
+    <div v-for="(day, index) in timetable" :key="index" class="day">
+      <div class="week-day">{{ daysOfTheWeek[index] }}</div>
+      <div v-for="row in day" :key="row.class + '_' + row.time" class="class">
+        <div class="time">{{ row.time }}</div>
+        <div class="class-name">{{ row.class }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  timetable: { time: string; class: string }[][]
+}>()
+
+const daysOfTheWeek: string[] = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье',
+]
+</script>
+
+<style scoped>
+.week {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.day {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.class {
+  display: flex;
+  gap: 10px;
+  margin: 0 5px;
+}
+
+.time {
+  width: 40px;
+}
+
+.week-day {
+  background: var(--secondary);
+  padding: 5px 10px;
+}
+</style>
--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -8,11 +8,24 @@
 
     <month-calendar :month="month" :year="year"></month-calendar>
   </div>
+  <div class="timetable">
+    <div class="timetable-controller">
+      <button @click.prevent="changeWeek(-1)">&lt;</button>
+      <a @click.prevent="resetWeek()" title="Вернуться на текущую неделю">{{
+        weekNames[selectedWeek]
+      }}</a>
+      <button @click.prevent="changeWeek(+1)">&gt;</button>
+    </div>
+
+    <classes-timetable :timetable="timetable[selectedWeek]"></classes-timetable>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import MonthCalendar from '@/components/MonthCalendar.vue'
+import ClassesTimetable from '@/components/ClassesTimetable.vue'
+import { getWeek } from '@/utils/calendar-date'
 
 const months: string[] = [
   'Январь',
@@ -29,7 +42,7 @@ const months: string[] = [
   'Декабрь',
 ]
 
-const currentDate: Date = new Date()
+let currentDate: Date = new Date()
 const year = ref(currentDate.getFullYear())
 const month = ref(currentDate.getMonth())
 
@@ -49,21 +62,109 @@ function changeMonth(offset: number): void {
 }
 
 function resetDate(): void {
-  const currentDate: Date = new Date()
+  currentDate = new Date()
   year.value = currentDate.getFullYear()
   month.value = currentDate.getMonth()
+}
+
+const timetable: { time: string; class: string }[][][] = [
+  [
+    [
+      { time: '9:45', class: 'АИСД' },
+      { time: '11:30', class: 'МЛИТА' },
+      { time: '13:25', class: 'Матан' },
+    ],
+    [
+      { time: '8:00', class: 'Алгем' },
+      { time: '9:45', class: 'Матан' },
+      { time: '13:25', class: 'История' },
+      { time: '15:10', class: 'Физра' },
+    ],
+    [
+      { time: '8:00', class: 'АИСД' },
+      { time: '9:45', class: 'История' },
+      { time: '11:30', class: 'WEBтех' },
+      { time: '13:15', class: 'Культурология' },
+      { time: '18:40', class: 'АВС' },
+    ],
+    [
+      { time: '9:45', class: 'Английский' },
+      { time: '11:30', class: 'Английский' },
+      { time: '13:25', class: 'АВС' },
+      { time: '15:10', class: 'Дискра' },
+      { time: '16:55', class: 'Физра' },
+    ],
+    [
+      { time: '8:00', class: 'Алгем' },
+      { time: '9:45', class: 'Алгем' },
+      { time: '13:25', class: 'Дискра' },
+    ],
+  ],
+
+  [
+    [{ time: '13:25', class: 'Матан' }],
+    [
+      { time: '8:00', class: 'Алгем' },
+      { time: '9:45', class: 'Матан' },
+      { time: '11:30', class: 'Английский' },
+      { time: '13:25', class: 'История' },
+      { time: '15:10', class: 'Физра' },
+    ],
+    [
+      { time: '8:00', class: 'АИСД' },
+      { time: '9:45', class: 'История' },
+      { time: '11:30', class: 'Си' },
+      { time: '13:15', class: 'Культурология' },
+      { time: '18:40', class: 'АВС' },
+    ],
+    [
+      { time: '8:00', class: 'Си' },
+      { time: '9:45', class: 'АИСД' },
+      { time: '11:30', class: 'Web-тех' },
+      { time: '13:25', class: 'АВС' },
+      { time: '15:10', class: 'Дискра' },
+      { time: '16:55', class: 'Физра' },
+    ],
+    [
+      { time: '8:00', class: 'Алгем' },
+      { time: '9:45', class: 'МЛИТА' },
+      { time: '13:25', class: 'Дискра' },
+    ],
+  ],
+]
+
+const selectedWeek = ref(0)
+const totalWeeks = timetable.length
+const weekNames: string[] = ['Числитель', 'Знаменатель']
+resetWeek()
+
+function changeWeek(offset: number): void {
+  let weekPostOffset = selectedWeek.value + offset
+  if (weekPostOffset >= totalWeeks) {
+    weekPostOffset = 0
+  } else if (weekPostOffset < 0) {
+    weekPostOffset = totalWeeks - 1
+  }
+
+  selectedWeek.value = weekPostOffset
+}
+
+function resetWeek(): void {
+  selectedWeek.value = (getWeek(currentDate, 1) + 1) % totalWeeks
 }
 </script>
 
 <style scoped>
-.calendar {
+.calendar,
+.timetable {
   display: inline-flex;
   flex-direction: column;
   align-items: center;
   gap: 15px;
 }
 
-.calendar-controller {
+.calendar-controller,
+.timetable-controller {
   display: flex;
   gap: 10px;
   justify-content: space-between;
@@ -74,14 +175,18 @@ function resetDate(): void {
   user-select: none; /* Standard */
 }
 
-.calendar-controller span {
+.calendar-controller span,
+.timetable-controller a {
   width: 120px;
   text-align: center;
   cursor: pointer;
+  color: var(--secondary);
 }
 
-.calendar-controller button {
-  background: none;
+.calendar-controller button,
+.timetable-controller button {
+  background: var(--primary);
+  border-radius: 5px;
   border: none;
   cursor: pointer;
   color: var(--text);
