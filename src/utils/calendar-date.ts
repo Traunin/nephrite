ca8a1514a92ddfbeee1a4c73620b598d0453fc53
Traunin <traunin5@gmail.com>
2024-05-09 23:34:48 +0300

    feat: add getWeek()

--- src/utils/calendar-date.ts
+++ src/utils/calendar-date.ts
@@ -35,3 +35,33 @@ export function isSameDate(date1: CalendarDate, date2: CalendarDate): boolean {
 export function getAsString(date: CalendarDate): string {
   return date.day + '_' + date.month + '_' + date.year
 }
+
+export function getWeek(date: Date, dowOffset: number): number {
+  dowOffset = typeof dowOffset === 'number' ? dowOffset : 0 // default dowOffset to zero
+  const newYear = new Date(date.getFullYear(), 0, 1)
+  let day = newYear.getDay() - dowOffset // the day of week the year begins on
+  day = day >= 0 ? day : day + 7
+  const daynum =
+    Math.floor(
+      (date.getTime() -
+        newYear.getTime() -
+        (date.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) /
+        86400000
+    ) + 1
+  let weeknum
+  // if the year starts before the middle of a week
+  if (day < 4) {
+    weeknum = Math.floor((daynum + day - 1) / 7) + 1
+    if (weeknum > 52) {
+      const nYear = new Date(date.getFullYear() + 1, 0, 1)
+      let nday = nYear.getDay() - dowOffset
+      nday = nday >= 0 ? nday : nday + 7
+      /* if the next year starts before the middle of
+                the week, it is week #1 of that year */
+      weeknum = nday < 4 ? 1 : 53
+    }
+  } else {
+    weeknum = Math.floor((daynum + day - 1) / 7)
+  }
+  return weeknum
+}
